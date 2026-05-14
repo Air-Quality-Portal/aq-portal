66041e4d404ca8a89aac9684cf6a57b89cde5d20
snmln <smalone@element84.com>
2026-05-14 13:32:55 -0400

    setting up section header

--- app/disasters-components/section-header/index.tsx
+++ app/disasters-components/section-header/index.tsx
@@ -1,3 +1,10 @@
 export const SectionHeader = ({ headline }: { headline: string }) => {
-  return <h2 className="font-sans-2xl padding-bottom-2 text-primary-dark">{headline}</h2>;
+  return (
+    <h2
+      className="font-sans-2xl padding-bottom-2 margin-0 text-primary-dark"
+      style={{ fontFamily: "'Inter', sans-serif" }}
+    >
+      {headline}
+    </h2>
+  );
 };
