a411eafbb8e748cc26e9d593cae75ce7047d92da
Alice Rühl <alice@developmentseed.org>
2026-05-20 12:14:41 +0200

    fix: set 2-col grid and card height lg on training page

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -12,8 +12,8 @@ export default function TrainingCollectionPage() {
       <section className="grid-container padding-y-6">
         <div className="grid-row grid-gap">
           {TRAINING_CARDS.map(({ id, ...card }) => (
-            <div key={id} className="grid-col-12 tablet:grid-col-6 desktop:grid-col-4">
-              <CardDetailed {...card} />
+            <div key={id} className="grid-col-12 tablet:grid-col-6">
+              <CardDetailed {...card} className="height-card-lg" />
             </div>
           ))}
         </div>
