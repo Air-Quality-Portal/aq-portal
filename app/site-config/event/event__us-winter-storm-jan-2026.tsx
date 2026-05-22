ecacfae5563da68ce4213c8a0426b9705dcd9791
Simi Damani <simi@developmentseed.org>
2026-05-22 16:43:04 -0700

    feat: add event content for respond page cards

---
+++ app/site-config/event/event__us-winter-storm-jan-2026.tsx
@@ -0,0 +1,17 @@
+import type { EventContent } from "@/app/site-config/types";
+
+export const EVENT__US_WINTER_STORM_EVENT_2026: EventContent = {
+  id: "us-winter-storm-jan-2026",
+  contentType: "event",
+  title: "Typhoon Sinlaku April 2026",
+  thumbnailImage: {
+    src: "/img/event/us-winter-storm-jan-2026.webp",
+    alt: "Satellite imagery shows the aftermath of a potent winter storm in late January 2026",
+  },
+  mastheadImage: {
+    src: "/img/event/us-winter-storm-jan-2026.webp",
+    alt: "Satellite imagery shows the aftermath of a potent winter storm in late January 2026",
+  },
+  themes: [],
+  categories: [],
+};
