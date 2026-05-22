a0db072b43419eab37be7b14f195c4886ae0c088
Simi Damani <simi@developmentseed.org>
2026-05-22 15:28:17 -0700

    feat: add data stories and news to news-events page

--- app/news-events/[id]/page.tsx
+++ app/news-events/[id]/page.tsx
@@ -1,11 +1,13 @@
 import { notFound } from "next/navigation";
 import { PageMasthead, PageStatus, Section } from "@/app/components/";
+import { DATASTORIES } from "@/app/site-config/datastory";
+import { NEWS } from "@/app/site-config/news";
 import { STORIES } from "@/app/site-config/story";
 
 export default async function NewsEventsItemPage(props: PageProps<"/news-events/[id]">) {
   const { id } = await props.params;
 
-  const story = STORIES.find((s) => s.id === id);
+  const story = [...STORIES, ...DATASTORIES, ...NEWS].find((s) => s.id === id);
 
   if (!story) notFound();
 
@@ -17,7 +19,7 @@ export default async function NewsEventsItemPage(props: PageProps<"/news-events/
 
       <Section className="margin-top-4 margin-bottom-0">
         <PageStatus
-          label={`Story Item: ${id}`}
+          label={`News, Data Story, or Story Item: ${id}`}
           heading="Under development"
           description="The page you're looking for is under development."
         />
