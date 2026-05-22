78b32841d25ddf796ac25328f2a25f28e1fb2b9c
Simi Damani <simi@developmentseed.org>
2026-05-22 11:41:29 -0700

    fix: add external training content to training gallery page

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -1,6 +1,6 @@
 import { CardDetailed } from "@teamimpact/veda-ui-blocks";
 import { PageMasthead, Section } from "@/app/components";
-import { TRAININGS } from "@/app/site-config/training";
+import { TRAININGS, TRAININGS_EXTERNAL } from "@/app/site-config/training";
 import { PAGE__CARD_MASTHEAD } from "@/app/site-config/training/page__card-masthead";
 import { makeCardDetailedProps } from "../site-config/content.helpers";
 
@@ -10,8 +10,8 @@ export default function TrainingCollectionPage() {
       <PageMasthead {...PAGE__CARD_MASTHEAD} />
       <Section>
         <div className="grid-row grid-gap">
-          {TRAININGS.map(({ id, categories, themes, ...card }) => (
-            <div key={id} className="grid-col-12 tablet:grid-col-6">
+          {[...TRAININGS, ...TRAININGS_EXTERNAL].map(({ id, categories, themes, ...card }) => (
+            <div key={id} className="grid-col-12 tablet:grid-col-6 margin-y-1 desktop:margin-y-2">
               <CardDetailed
                 {...makeCardDetailedProps({ ...card, id })}
                 className="height-card-lg"
