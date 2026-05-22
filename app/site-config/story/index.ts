5725d7beec12892fe016da9cc55179b51f6aa3d0
Simi Damani <simi@developmentseed.org>
2026-05-22 12:38:36 -0700

    feat: add clearing-the-way-debris-mapping story for recover page

--- app/site-config/story/index.ts
+++ app/site-config/story/index.ts
@@ -1,5 +1,10 @@
 import type { StoryContent } from "@/app/site-config/types";
+import { STORY__CLEARING_THE_WAY_DEBRIS_MAPPING } from "./story__clearing-the-way-debris-mapping";
 import { STORY__ESTIMATING_LOSS_RECOVERY } from "./story__estimating-loss-recovery";
 import { STORY__FINDING_FLOODS } from "./story__finding-floods";
 
-export const STORIES: StoryContent[] = [STORY__ESTIMATING_LOSS_RECOVERY, STORY__FINDING_FLOODS];
+export const STORIES: StoryContent[] = [
+  STORY__ESTIMATING_LOSS_RECOVERY,
+  STORY__FINDING_FLOODS,
+  STORY__CLEARING_THE_WAY_DEBRIS_MAPPING,
+];
