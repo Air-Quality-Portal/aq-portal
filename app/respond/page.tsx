fcf0008c739160ddb5e85b0bf16cb128f455bafb
Simi Damani <simi@developmentseed.org>
2026-05-25 17:42:49 -0700

    feat: add respond page content and page

--- app/respond/page.tsx
+++ app/respond/page.tsx
@@ -1,5 +1,19 @@
-import { PageMasthead, PageStatus } from "@/app/components/";
-import { RESPOND_CONTENT } from "../site-config/theme/theme__respond";
+import {
+  PageMasthead,
+  Section,
+  SectionCardCarousel,
+  SectionCardSimple,
+  SectionCardSimpleMosaic,
+  SectionHeading,
+} from "@/app/components/";
+import { makeCardCarouselProps, makeCardSimpleProps } from "../site-config/content.helpers";
+import {
+  RESPOND_CONTENT,
+  RESPOND_DATASTORIES,
+  RESPOND_STORIES,
+  RESPOND_TRAININGS,
+} from "../site-config/theme/theme__respond";
+import { typedMap } from "../site-config/typed.helpers";
 
 export default function RespondPage() {
   const { title, theme, subtitle, mastheadImage } = RESPOND_CONTENT;
@@ -7,10 +21,22 @@ export default function RespondPage() {
   return (
     <>
       <PageMasthead {...{ title, subtitle, theme, mastheadImage }} />
-      <PageStatus
-        label="Respond"
-        heading="Under development"
-        description="The page you're looking for is under development."
+      <SectionCardSimpleMosaic
+        sectionHeading={<SectionHeading href="/news-events">News & Events</SectionHeading>}
+        cards={typedMap(RESPOND_STORIES, makeCardSimpleProps)}
+      />
+      {/* Data Visualization */}
+      <Section>
+        <SectionHeading>Data Visualization</SectionHeading>
+        <p>TODO: Map block</p>
+      </Section>
+      <SectionCardSimple
+        sectionHeading={<SectionHeading href="/training">Resources & Learning</SectionHeading>}
+        cards={typedMap(RESPOND_TRAININGS, makeCardSimpleProps)}
+      />
+      <SectionCardCarousel
+        sectionHeading={<SectionHeading href="/stories">Data Stories</SectionHeading>}
+        cards={typedMap(RESPOND_DATASTORIES, makeCardCarouselProps)}
       />
     </>
   );
