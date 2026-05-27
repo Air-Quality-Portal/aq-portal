025d995721909372439d4330869d791f9b793554
Simi Damani <simi@developmentseed.org>
2026-05-27 10:28:41 -0700

    fix: update masthead definition on news and events gallery after changes from main

--- app/news-events/page.tsx
+++ app/news-events/page.tsx
@@ -1,17 +1,20 @@
 import { CardDetailed } from "@teamimpact/veda-ui-blocks";
 import { PageMasthead, Section } from "@/app/components";
+import {
+  makeCardDetailedImageLeftProps,
+  makeCardMastHeadProps,
+} from "@/app/site-config/content.helpers";
 import { DATASTORIES } from "@/app/site-config/datastory";
 import { NEWS } from "@/app/site-config/news";
 import { NEWS_EVENTS_CARD_MASTHEAD } from "@/app/site-config/news-events/news-events-card-masthead";
 import { STORIES } from "@/app/site-config/story";
-import { makeCardDetailedImageLeftProps } from "../site-config/content.helpers";
 
 export default function NewsEventsCollectionPage() {
   const items = [...NEWS, ...STORIES, ...DATASTORIES];
 
   return (
     <>
-      <PageMasthead {...NEWS_EVENTS_CARD_MASTHEAD} />
+      <PageMasthead {...makeCardMastHeadProps(NEWS_EVENTS_CARD_MASTHEAD)} />
       <Section>
         <div className="grid-row grid-gap">
           {items.map(({ id, thumbnailImage, contentType, categories, themes, ...card }) => (
--- app/site-config/news-events/news-events-card-masthead.ts
+++ app/site-config/news-events/news-events-card-masthead.ts
@@ -1,5 +1,5 @@
 export const NEWS_EVENTS_CARD_MASTHEAD = {
-  image: { alt: "", src: "/img/card-masthead.webp" },
+  mastheadImage: { alt: "", src: "/img/card-masthead.webp" },
   title: "News & Events",
   description:
     "Stay informed with the latest news, stories, and data stories from the NASA Disasters Program",
