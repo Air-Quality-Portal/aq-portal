94fd19e3ef6cb324569d96e3b045f0668932ddfc
Simi Damani <simi@developmentseed.org>
2026-05-27 19:05:26 -0700

    fix: rename to make card count more generic

--- app/components/component.helpers.ts
+++ app/components/component.helpers.ts
@@ -1,7 +1,7 @@
-export const getGridColumnClass = (cardCount: number, maxColumns: 1 | 2 | 3 | 4 = 4) => {
-  if (maxColumns >= 4 && cardCount % 4 === 0)
+export const getGridColumnClass = (itemCount: number, maxColumns: 1 | 2 | 3 | 4 = 4) => {
+  if (maxColumns >= 4 && itemCount % 4 === 0)
     return "grid-col-12 tablet:grid-col-6 desktop:grid-col-3";
-  if (maxColumns >= 3 && cardCount % 3 === 0) return "grid-col-12 tablet:grid-col-4";
-  if (maxColumns >= 2 && cardCount % 2 === 0) return "grid-col-12 tablet:grid-col-6";
+  if (maxColumns >= 3 && itemCount % 3 === 0) return "grid-col-12 tablet:grid-col-4";
+  if (maxColumns >= 2 && itemCount % 2 === 0) return "grid-col-12 tablet:grid-col-6";
   return "grid-col-12";
 };
