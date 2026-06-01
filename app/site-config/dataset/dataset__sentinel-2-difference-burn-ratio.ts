ebafae3861a8953db9ea51d47a78700c013d2fce
Sean Malone <33710544+snmln@users.noreply.github.com>
2026-06-01 12:12:38 -0400

    Apply suggestion from @snmln

--- app/site-config/dataset/dataset__sentinel-2-difference-burn-ratio.ts
+++ app/site-config/dataset/dataset__sentinel-2-difference-burn-ratio.ts
@@ -7,7 +7,7 @@ export const DATASET__SENTINEL_2_DIFFERENCE_BURN_RATIO: DatasetContent = {
   description:
     "Differenced Normalized Burn Ratio (dNBR) is computed by the difference between the pre-fire NBR and the post-fire NBR. Since the dNBR considers the condition of the scene before the fire occurred, the resulting value has been used as a proxy for burn severity.",
   thumbnailImage: {
-    src: "/img/dataset/sentinel-2-nbr.webp",
+    src: "/img/dataset/sentinel-2-difference-burn-ratio.webp",
     alt: "Sentinel-2 Differenced Normalized Burn Ratio (dNBR) imagery example",
   },
   mastheadImage: {
