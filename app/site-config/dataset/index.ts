ee914632207d9fcba9ce2f9708dd76bf1424dc4c
Simi Damani <simi@developmentseed.org>
2026-06-03 22:52:07 -0700

    feat: add dataset sentinel 2 dnbr details and image

--- app/site-config/dataset/index.ts
+++ app/site-config/dataset/index.ts
@@ -1,5 +1,6 @@
 import type { DatasetContent } from "@/app/site-config/types";
 import { DATASET__SENTINEL_2_COLOR_INFRARED } from "./dataset__sentinel-2-color-infrared";
+import { DATASET__SENTINEL_2_DNBR } from "./dataset__sentinel-2-dnbr";
 import { DATASET__SENTINEL_2_SWIR } from "./dataset__sentinel-2-swir";
 import { DATASET__SENTINEL_2_TRUE_COLOR } from "./dataset__sentinel-2-true-color";
 
@@ -7,4 +8,5 @@ export const DATASETS: DatasetContent[] = [
   DATASET__SENTINEL_2_TRUE_COLOR,
   DATASET__SENTINEL_2_COLOR_INFRARED,
   DATASET__SENTINEL_2_SWIR,
+  DATASET__SENTINEL_2_DNBR,
 ];
