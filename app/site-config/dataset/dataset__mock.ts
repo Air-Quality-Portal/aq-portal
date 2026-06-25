234c3a926a2ce681f025b7eb1cd5259afaea3590
Ram Sharan Rimal <rsr0012@uah.edu>
2026-06-25 16:10:14 -0500

    chore: add sample image for mock dataset

--- app/site-config/dataset/dataset__mock.ts
+++ app/site-config/dataset/dataset__mock.ts
@@ -7,12 +7,12 @@ export const DATASET__MOCK: DatasetContent = {
   description:
     "The True Color RGB composite provides a product of how the surface would look to the naked eye from space.",
   thumbnailImage: {
-    src: "https://picsum.photos/id/237/200/300",
-    alt: "Sample Text",
+    src: "/img/dataset/sentinel-2-true-color.webp",
+    alt: "Sentinel-2 True Color imagery example",
   },
   mastheadImage: {
-    src: "https://picsum.photos/id/237/1080/1280",
-    alt: "Sample Text",
+    src: "/img/dataset/sentinel-2-true-color.webp",
+    alt: "Sentinel-2 True Color imagery example",
   },
   category1: ["c1 lorem", "c1 dore"],
   category2: ["c2 lorem", "c2 dore"],
