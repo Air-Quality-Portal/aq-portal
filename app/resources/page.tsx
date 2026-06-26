323923d282a1faaca4a24f51ef2a6e15d6a15daf
Rob Gomez <rgomez@element84.com>
2026-06-26 14:10:57 -0500

    use contentBlock render

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,20 +1,15 @@
-import { PageMasthead, Section } from "@/app/components";
-import { RESOURCES_PAGE_MASTHEAD } from "@/app/site-config/resources";
+import { ContentBlockRenderer, PageMasthead } from "@/app/components";
+import { RESOURCES_PAGE_BODY, RESOURCES_PAGE_MASTHEAD } from "@/app/site-config/resources";
 import { makeCardMastHeadProps } from "../site-config/content.helpers";
 
 export default function ResourcesPage() {
   return (
     <>
       <PageMasthead {...makeCardMastHeadProps(RESOURCES_PAGE_MASTHEAD)} />
-      <Section>
-        <div className="grid-row">
-          <div className="grid-col-12">
-            <p className="font-sans-md text-base-dark margin-y-0">
-              Training resources placeholder content.
-            </p>
-          </div>
-        </div>
-      </Section>
+      {RESOURCES_PAGE_BODY.body.map((block, i) => (
+        // biome-ignore lint/suspicious/noArrayIndexKey: static content blocks, never reorder
+        <ContentBlockRenderer key={i} block={block} />
+      ))}
     </>
   );
 }
