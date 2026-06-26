9cc71baa6c896450e526bce21b7626d22806bac8
Rob Gomez <rgomez@element84.com>
2026-06-26 14:09:43 -0500

    removed exports

--- app/site-config/resources/index.ts
+++ app/site-config/resources/index.ts
@@ -1,5 +1,3 @@
+export type { ResourcesPageBody } from "./resources__page";
+export { RESOURCES_PAGE_BODY } from "./resources__page";
 export { RESOURCES_PAGE_MASTHEAD } from "./toplevel-page__card-masthead";
-
-export const RESOURCES_DOCUMENTATION: unknown[] = [];
-export const RESOURCES_DATASETS: unknown[] = [];
-export const RESOURCES_TOOLS: unknown[] = [];
