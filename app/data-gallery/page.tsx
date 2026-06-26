5cea3b7f1cb9b047f1e92e5bd37b73c6672cabd4
Ram Sharan Rimal <rsr0012@uah.edu>
2026-06-26 11:04:46 -0500

    feat: arrange cards in data catalog in grids

--- app/data-gallery/page.tsx
+++ app/data-gallery/page.tsx
@@ -1,10 +1,7 @@
 import { CardDetailed } from "@teamimpact/veda-ui-blocks";
 import { PageMasthead, Section } from "@/app/components";
 import { DATA_GALLERY_CARD_MASTHEAD } from "@/app/site-config/dataset/toplevel-page__card-masthead";
-import {
-  makeCardDetailedImageLeftProps,
-  makeCardMastHeadProps,
-} from "../site-config/content.helpers";
+import { makeCardDetailedProps, makeCardMastHeadProps } from "../site-config/content.helpers";
 import { DATASETS } from "../site-config/dataset";
 
 export default function DataGalleryPage() {
@@ -14,16 +11,16 @@ export default function DataGalleryPage() {
       <Section>
         <div className="grid-row grid-gap">
           {DATASETS.map(({ id, thumbnailImage, category1, category2, category3, ...card }) => (
-            <div key={id} className="grid-col-12 margin-y-1 desktop:margin-y-2">
+            <div key={id} className="grid-col-12 tablet:grid-col-6 margin-y-1 desktop:margin-y-2">
               <CardDetailed
-                {...makeCardDetailedImageLeftProps({
+                {...makeCardDetailedProps({
                   id,
                   thumbnailImage,
                   tags: [...category1, ...category2, ...category3],
                   tagPrimary: category1[0],
                   ...card,
                 })}
-                className="height-card-sm"
+                className="height-card-lg"
               />
             </div>
           ))}
