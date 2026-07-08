6191d5cc91fc7e2e603a8fe1a2643d4289d1d847
Bhawana Karakheti <bhawanakarakheti2019@gmail.com>
2026-07-08 14:13:30 -0500

    fix: reverted padding changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,17 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="display-flex flex-column minh-viewport">
         <Header {...MOCK_HEADER_PROPS} />
-        <main
-          className="flex-1"
-          style={{
-            paddingLeft: "220px",
-            paddingRight: "220px",
-            paddingTop: "41px",
-            paddingBottom: "71px",
-          }}
-        >
-          {children}
-        </main>
+        <main className="flex-1">{children}</main>
         <Footer {...MOCK_FOOTER_PROPS} />
       </body>
     </html>
