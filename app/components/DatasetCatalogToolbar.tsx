39030d483b2972f73ffe237a3b0b087fd1ef6b5c
Ram Sharan Rimal <rsr0012@uah.edu>
2026-07-27 18:41:05 -0500

    fix: toolbar styles of datacatalog page

--- app/components/DatasetCatalogToolbar.tsx
+++ app/components/DatasetCatalogToolbar.tsx
@@ -4,10 +4,10 @@ type DatasetCatalogToolbarProps = {
 };
 
 export const DatasetCatalogToolbar = ({ count }: DatasetCatalogToolbarProps) => (
-  <div className="display-flex flex-justify flex-align-center flex-wrap margin-bottom-3">
+  <div className="display-flex flex-justify flex-align-center flex-wrap margin-top-2 margin-bottom-1 border border-base-lighter radius-lg bg-white padding-y-105 padding-x-205">
     <p className="display-flex flex-align-center margin-0 text-bold">
       Datasets
-      <span className="margin-left-1 padding-x-1 bg-primary text-white radius-md font-sans-2xs">
+      <span className="margin-left-1 padding-x-1 padding-y-1 bg-primary text-white radius-md font-sans-2xs">
         {count}
       </span>
     </p>
