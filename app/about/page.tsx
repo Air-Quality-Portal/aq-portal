2ceba31b5a35a900848fa7acbfd58dfd84e250e1
Bhawana Karakheti <bhawanakarakheti2019@gmail.com>
2026-08-05 22:29:08 -0500

    Fix: use correct InPageNavigation props and scope to content area

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,24 @@
 import { Card, CardContact, InPageNavigation } from "@teamimpact/veda-ui-blocks";
-import { ContentBlockRenderer } from "@/app/components/ContentBlockRenderer";
+import { ContentBlockRenderer, ContentHeading, Section } from "@/app/components";
 import { ABOUT_PAGE_BODY } from "@/app/site-config/about/about__page";
-import { Section } from "../components";
 
 const ABOUT_CONTENT_ID = "about-page-content";
 
 export default function AboutPage() {
   const { body, contacts } = ABOUT_PAGE_BODY;
-  const HeadingTag = contacts?.headingLevel ?? "h3";
 
   return (
     <Section>
-      <Card className=" height-masthead" isMastHead title={"About AIR4US"} />
+      <Card className="height-masthead" isMastHead title={"About AIR4US"} />
 
       <div className="grid-container padding-y-7">
         <div className="grid-row grid-gap">
           <div className="grid-col-3 display-none desktop:display-block">
-            <InPageNavigation title="On THIS PAGE" contentSelector={`#${ABOUT_CONTENT_ID}`} />
+            <InPageNavigation
+              title="On THIS PAGE"
+              contentSelector={`#${ABOUT_CONTENT_ID}`}
+              headingElements="h3"
+            />
           </div>
           <div className="grid-col-9 usa-prose" id={ABOUT_CONTENT_ID}>
             {body.map((block, i) => (
@@ -26,9 +28,7 @@ export default function AboutPage() {
             {contacts && (
               <Section>
                 {contacts.heading && (
-                  <HeadingTag className="font-heading-lg margin-bottom-1">
-                    {contacts.heading}
-                  </HeadingTag>
+                  <ContentHeading heading={contacts.heading} headingLevel={contacts.headingLevel} />
                 )}
                 {contacts.lead && <p>{contacts.lead}</p>}
                 <div className="margin-top-2">
