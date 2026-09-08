950170f6163e9d3f5d632c8e97596761e8bf684a
Ram Sharan Rimal <rsr0012@uah.edu>
2026-09-08 11:37:46 -0500

    fix: move search inside dataset toolbar

--- app/components/DatasetCatalogToolbar.tsx
+++ app/components/DatasetCatalogToolbar.tsx
@@ -9,20 +9,18 @@ type DatasetCatalogToolbarProps = {
 };
 
 export const DatasetCatalogToolbar = ({ count, query = "" }: DatasetCatalogToolbarProps) => (
-  <div className="margin-y-6">
+  <div className="display-flex flex-justify flex-align-center flex-wrap margin-y-6 border border-base-lighter radius-lg bg-white padding-y-105 padding-x-205">
+    <p aria-live="polite" className="display-flex flex-align-center margin-0 text-bold">
+      Datasets
+      <span className="margin-left-1 padding-x-1 padding-y-1 bg-primary text-white radius-md font-sans-2xs">
+        {count}
+      </span>
+    </p>
     <CatalogSearchInput
       query={query}
       label="Search datasets"
       placeholder="Search datasets..."
       inputId="dataset-catalog-search"
     />
-    <div className="display-flex flex-justify flex-align-center flex-wrap margin-top-2 border border-base-lighter radius-lg bg-white padding-y-105 padding-x-205">
-      <p aria-live="polite" className="display-flex flex-align-center margin-0 text-bold">
-        Datasets
-        <span className="margin-left-1 padding-x-1 padding-y-1 bg-primary text-white radius-md font-sans-2xs">
-          {count}
-        </span>
-      </p>
-    </div>
   </div>
 );
