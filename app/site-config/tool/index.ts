b1b6d0abda35428bb77d7023febc0f14dcaf7de4
Bhawana Karakheti <bhawanakarakheti2019@gmail.com>
2026-09-15 14:44:37 -0500

    feat: add tool catalog content, images, and attribution data

--- app/site-config/tool/index.ts
+++ app/site-config/tool/index.ts
@@ -1,6 +1,6 @@
-import { TOOLS } from "./tools__mock";
+import { TOOLS } from "./tools__content";
 
-export { TOOLS } from "./tools__mock";
+export { TOOLS } from "./tools__content";
 export { AIR4US_TOOL_INTRO, PARTNER_TOOLS_INTRO } from "./tools__page";
 export { searchTools } from "./tools__search";
 
